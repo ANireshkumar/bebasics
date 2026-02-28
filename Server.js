d9354348620a64e95468fb97d8feef0afe4bf307
Niresh Kumar A <niresh1606@gmail.com>
2026-02-28 17:53:37 +0530

    custom middleware and all reqests

--- Server.js
+++ Server.js
@@ -4,10 +4,38 @@ const express = require('express');
 // creating the express app
 const app = express();
 
+// a middleware  function to log the request method and url
+const logger = (req,res,next)=>{
+    console.log(`${req.method} ${req.url}`);
+    console.log("Request received at:", new Date().toISOString());
+       console.log(`${req.body}`);
+       console.log("----------------------")
+
+    // pass 
+    next();
+}   
+
+// use the logger middleware for all routes
+app.use(logger);
+
 //request handler for the root route
 app.get('/',(req,res)=>{
     res.json`({message:"Welcome to the Express Server!"})`;
 });
+
+app.post('/',(req,res)=>{
+    res.json({message:"POST request received!"});
+});
+
+app.put('/',(req,res)=>{
+    res.json({message:"PUT request received!"});
+});
+app.delete('/',(req,res)=>{
+    res.json({message:"DELETE request received!"});
+});
+
+
+
 
 
 // listening to the request 
