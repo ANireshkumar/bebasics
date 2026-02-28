b055055b3df824c1094564691948f184d9d3ea6b
Niresh Kumar A <niresh1606@gmail.com>
2026-02-28 19:33:26 +0530

    splited to server + app

---
+++ app.js
@@ -0,0 +1,31 @@
+//import the express module
+const express = require('express');
+
+// creating the express app
+const app = express();
+
+// importing the morgan middleware for logging
+const morgan = require('morgan');
+
+// use morgan middleware for logging
+app.use(morgan('dev'));
+
+
+
+//request handler for the root route
+app.get('/',(req,res)=>{
+    res.json({message:"Welcome to the Express Server!"});
+});
+
+app.post('/',(req,res)=>{
+    res.json({message:"POST request received!"});
+});
+
+app.put('/',(req,res)=>{
+    res.json({message:"PUT request received!"});
+});
+app.delete('/',(req,res)=>{
+    res.json({message:"DELETE request received!"});
+});
+
+module.exports = app;
