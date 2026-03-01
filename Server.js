60320b1d96cba9bf8cf81ecc6d90bc04f4b9fd32
Niresh Kumar A <niresh1606@gmail.com>
2026-03-01 07:31:01 +0530

    connected mangodb locally&removed all testroutes

--- Server.js
+++ Server.js
@@ -2,10 +2,17 @@
 const app = require('./app.js');
 
 
+// import the mangoose
+const mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/testdb')
+    .then(() => console.log('Database connected successfully'))
+    .catch((err) => console.log('Database connection error:', err));
 
 
 // listening to the request 
 
 app.listen(30001,"localhost",()=>{
     console.log("Server is running on port http://localhost:30001");
-});
+})
--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const morgan = require('morgan');
 // use morgan middleware for logging
 app.use(morgan('dev'));
 
-// impot the test router
-const testRouter = require('./Routes/testRoutes.js');
 
-app.use('/api/v1/test',testRouter);
 
 // prefex rout /api/v1/test
 
