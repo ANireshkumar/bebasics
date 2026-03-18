9e396564bcbd412882f178974ee88c6d12161d55
Niresh Kumar A <niresh1606@gmail.com>
2026-03-18 06:02:24 +0530

    user regester tested- password not hidden

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,13 +1,27 @@
 //all function related to authentication will be here
 
+const User = require('../Models/user');
+
 const authController = {
     register: async (req, res) => {
         //logic for registering a user
-        res.status(201).json({ message: 'User registered successfully' });
+        
         try {
-            
+            const { username, email, password } = req.body;
+
+            // Check if the user already exists
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
+                return res.status(400).json({ message: 'User already exists' });
+            }
+
+            // Create a new user
+            const newUser = new User({ username, email, password });
+            await newUser.save();
+
+            res.status(201).json({ message: 'User registered successfully' });
         } catch (error) {
-            res.status(500).json({ message: 'Error registering user', error });
+            res.status(500).json({ message: 'Error registering user', error: error.message });
         }
     },
     login: async(req, res) => {
