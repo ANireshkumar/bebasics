d325a02bce37ee84c938e37850c67db7112e5794
Niresh Kumar A <niresh1606@gmail.com>
2026-03-18 06:13:53 +0530

    user registered - password hashed

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,6 +1,7 @@
 //all function related to authentication will be here
 
-const User = require('../Models/user');
+const User = require('../Models/user')
+const bcrypt = require('bcrypt');
 
 const authController = {
     register: async (req, res) => {
@@ -15,13 +16,16 @@ const authController = {
                 return res.status(400).json({ message: 'User already exists' });
             }
 
+            //hash password
+            const hashpassword = await bcrypt.hash(password, 10);
+
             // Create a new user
-            const newUser = new User({ username, email, password });
+            const newUser = new User({ username, email, password: hashpassword });
             await newUser.save();
 
             res.status(201).json({ message: 'User registered successfully' });
         } catch (error) {
-            res.status(500).json({ message: 'Error registering user', error: error.message });
+            res.status(500).json({ message: 'Error registering user', error });
         }
     },
     login: async(req, res) => {
